ae8bca85c215865eeacf28a2de0021bed1034158
mosidrum <mosiokanga@gmail.com>
2023-06-09 18:32:15 +0100

    Make changes from reviewer

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import Newbook from './Newbook';
+import Book from './Book';
+import DefaultProps from './DefaultProps';
 
-const BookList = () => (
-  <div className="booklist">
-    Book List
-    <Newbook />
-  </div>
-);
+const BookList = () => {
+  const Books = [
+    {
+      title: 'Book 1',
+      author: 'Mr man',
+    },
+    {
+      title: 'Book 2',
+      author: 'Mr woman',
+    },
+    {
+      title: 'Book 3',
+      author: 'Mr manwoman',
+    },
+  ];
+  /* eslint-disable react/no-array-index-key */
+  return (
+    <div className="booklist">
+      <ul>
+        {Books.map((book, index) => <Book key={index} title={book.title} author={book.author} />)}
+      </ul>
+      <Newbook />
+    </div>
+  );
+};
+
+BookList.prototype = DefaultProps;
 
 export default BookList;
---
+++ src/components/DefaultProps.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+const DefaultProps = {
+  title: PropTypes.string,
+  author: PropTypes.string,
+  key: PropTypes.number,
+};
+
+export default DefaultProps;
