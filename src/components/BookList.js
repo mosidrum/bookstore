d7300ea7d85c02fbd837f9f8caeed3620825931c
mosidrum <mosiokanga@gmail.com>
2023-06-13 07:43:08 +0100

    Add and remove books

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,33 +1,14 @@
 import React from 'react';
 import Newbook from './Newbook';
-import Book from './Book';
 import DefaultProps from './DefaultProps';
+import Book from './Book';
 
-const BookList = () => {
-  const Books = [
-    {
-      title: 'Book 1',
-      author: 'Mr man',
-    },
-    {
-      title: 'Book 2',
-      author: 'Mr woman',
-    },
-    {
-      title: 'Book 3',
-      author: 'Mr manwoman',
-    },
-  ];
-  /* eslint-disable react/no-array-index-key */
-  return (
-    <div className="booklist">
-      <ul>
-        {Books.map((book, index) => <Book key={index} title={book.title} author={book.author} />)}
-      </ul>
-      <Newbook />
-    </div>
-  );
-};
+const BookList = () => (
+  <div className="booklist">
+    <Book />
+    <Newbook />
+  </div>
+);
 
 BookList.prototype = DefaultProps;
 
